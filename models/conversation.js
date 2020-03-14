c1757bebf98374a20e7bc217ff8acd534c5fe3f4
hirosume <cuongvn@inet.vn>
2020-03-14 21:21:08 +0700

    decrease query time

--- models/conversation.js
+++ models/conversation.js
@@ -1,33 +1,68 @@
 /*
  * Copyright (c) 2020.
  * Author: hirosume.
- * LastModifiedAt: 3/11/20, 10:01 PM.
+ * LastModifiedAt: 3/14/20, 9:05 PM.
  */
 
 const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-    members: {
-        type: [String],
-        default: []
+const schema = new mongoose.Schema(
+    {
+        members: {
+            type: [String],
+            default: []
+        },
+        end: {
+            type: Boolean,
+            default: false
+        }
     },
-    end: {
-        type: Boolean,
-        default: false
+    {
+        timestamps: true
     }
-}, {
-    timestamps: true
-});
+);
+const map = new Map();
+function cache(members, obj) {
+    for (let psid of members) {
+        map.set(psid, obj);
+    }
+}
+function clear(members) {
+    for (let psid of members) {
+        map.delete(psid);
+    }
+}
 schema.statics.getAliveConversation = function(psid) {
-    return this.findOne({ end: false, members: psid }).exec();
+    if (map.has(psid)) {
+        return map.get(psid);
+    }
+    return this.findOne({ end: false, members: psid })
+        .exec()
+        .then(res => {
+            cache(res.members, res);
+            return res;
+        });
 };
 schema.statics.leaveConversationWithPsid = function(psid) {
-    return this.findOneAndUpdate({ end: false, members: psid }, { end: true }).exec();
+    return this.findOneAndUpdate({ end: false, members: psid }, { end: true })
+        .exec()
+        .then(res => {
+            clear(res.members);
+            return res;
+        });
 };
 schema.statics.leaveConversation = function(id) {
-    return this.findOneAndUpdate({ end: false, _id: id }, { end: true }).exec();
+    return this.findOneAndUpdate({ end: false, _id: id }, { end: true })
+        .exec()
+        .then(res => {
+            clear(res.members);
+            return res;
+        });
 };
 schema.statics.createConversation = function(members) {
-    return this.create({ members });
+    return this.create({ members }).then(res => {
+        cache(members, res);
+        return res;
+    });
 };
 const model = mongoose.model('conversation', schema);
 module.exports = model;
--- models/user.js
+++ models/user.js
@@ -1,62 +1,73 @@
 /*
  * Copyright (c) 2020.
  * Author: hirosume.
- * LastModifiedAt: 3/12/20, 6:07 PM.
+ * LastModifiedAt: 3/14/20, 8:51 PM.
  */
 
 const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-    psid: String,
-    name: String,
-    first_name: String,
-    last_name: String,
-    profile_pic: String,
-    gender: {
-        type: String,
-        default: 'unknown'
+const schema = new mongoose.Schema(
+    {
+        psid: String,
+        name: String,
+        first_name: String,
+        last_name: String,
+        profile_pic: String,
+        gender: {
+            type: String,
+            default: 'unknown'
+        },
+        lastSetGender: Date,
+        queuing: {
+            type: Boolean,
+            default: false
+        },
+        //report and block
+        block: {
+            default: false,
+            type: Boolean
+        },
+        blockDetail: {
+            type: String,
+            default: ''
+        },
+        reportedTimes: {
+            type: Number,
+            default: 0
+        },
+        reportTimes: {
+            type: Number,
+            default: 0
+        },
+        reportedBy: {
+            type: [String],
+            default: []
+        },
+        lastReport: {
+            type: Date,
+            default: Date.now
+        }
     },
-    lastSetGender: Date,
-    queuing: {
-        type: Boolean,
-        default: false
-    },
-    //report and block
-    block: {
-        default: false,
-        type: Boolean
-    },
-    blockDetail: {
-        type: String,
-        default: ''
-    },
-    reportedTimes: {
-        type: Number,
-        default: 0
-    },
-    reportTimes: {
-        type: Number,
-        default: 0
-    },
-    reportedBy: {
-        type: [String],
-        default: []
-    },
-    lastReport: {
-        type: Date,
-        default: Date.now
-    }
-}, { timestamps: true });
+    { timestamps: true }
+);
+const map = new Map();
 schema.statics.firstOrCreate = async function(payload) {
-    let user = await this.findOne({ psid: payload.psid }).exec();
+    const psid = payload.psid;
+    if (map.has(psid)) {
+        return map.get(psid);
+    }
+    let user = await this.findOne({ psid }).exec();
     if (!user) {
         user = await this.create(payload);
     }
+    map.set(psid, user);
     return user;
 };
 schema.statics.setQueue = function(psid) {
+    map.delete(psid);
     return this.findOneAndUpdate({ psid }, { queuing: true }).exec();
 };
 schema.statics.setNotQueue = function(psid) {
+    map.delete(psid);
     return this.findOneAndUpdate({ psid }, { queuing: false }).exec();
 };
 schema.statics.findFriend = function(psid, gender) {
@@ -74,6 +85,7 @@ schema.statics.findAndReport = async function(psid) {
         user.lastReport = new Date();
         user.reportedTimes++;
         user.reportedBy.push(psid);
+        map.delete(psid);
         return user.save();
     }
     return undefined;
@@ -82,6 +94,7 @@ schema.methods.isBlock = function() {
     return this.block;
 };
 schema.methods.blockMe = function(reason) {
+    map.delete(this.psid);
     this.block = true;
     this.blockDetail = reason;
     return this.save();
@@ -97,6 +110,7 @@ schema.methods.canReport = async function() {
         if (isInDifferenceDay(lDate, nDate)) {
             this.reportTimes = 0;
             this.lastReport = new Date();
+            map.delete(this.psid);
             await this.save();
         }
     }
@@ -104,11 +118,16 @@ schema.methods.canReport = async function() {
 };
 
 function isInDifferenceDay(lDate, nDate) {
-    return lDate.getFullYear() !== nDate.getFullYear() || lDate.getUTCMonth() !== nDate.getUTCMonth() || lDate.getDate() !== nDate.getDate();
+    return (
+        lDate.getFullYear() !== nDate.getFullYear() ||
+        lDate.getUTCMonth() !== nDate.getUTCMonth() ||
+        lDate.getDate() !== nDate.getDate()
+    );
 }
 
 schema.methods.reported = function() {
     this.reportTime++;
+    map.delete(this.psid);
     return this.save();
 };
 
